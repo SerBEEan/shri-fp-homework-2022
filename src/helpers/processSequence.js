ed124921f6c84acaa0a0ba641cfafb9fb129ab33
SerBEEan <sergey.atwork7@gmail.com>
2022-09-02 00:22:12 +0300

    сделал задачу 2

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,3 +1,7 @@
+import { __, lt, prop, compose, allPass, invoker, not, test, ifElse, modulo, pipe, curry, tap } from 'ramda';
+import { round } from 'lodash';
+import Api from '../tools/api';
+
 /**
  * @file Домашка по FP ч. 2
  *
@@ -14,38 +18,62 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const CORRECT_FORMAT_STRING = /^[\d.]+$/;
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const getValue = prop('value');
+const getWriteLog = prop('writeLog');
+const getHandleSuccess = prop('handleSuccess');
+const getHandleError = prop('handleError');
+const getLength = prop('length');
+const getResult = prop('result');
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const isMoreTowSymbols = compose(lt(2), getLength);
+const isLessTenSymbols = compose(lt(__, 10), getLength);
+const isCorrectFormatString = test(CORRECT_FORMAT_STRING);
+const isValueValid = allPass([isMoreTowSymbols, isLessTenSymbols, isCorrectFormatString]);
+const isValueInvalid = compose(not, isValueValid);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const tube = x => x;
+const powSquared = x => x ** 2;
+const throwValidationError = () => {throw 'ValidationError';}
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const getPromiseResolve = value => invoker(1, 'resolve')(value)(Promise);
+const then = invoker(1, 'then');
+const thenWithCatch = invoker(2, 'then');
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const checkInvalidString = ifElse(isValueInvalid, throwValidationError, tube);
+const makeResponse = (path, options) => invoker(2, 'get')(path, options)(api);
+const makeBinaryNumberResponse = number => makeResponse('https://api.tech/numbers/base', {from: 10, to: 2, number});
+const makeAnimalNameResponse = id => makeResponse(`https://animals.tech/${id}`, {});
 
- export default processSequence;
+const getFieldFromProps = curry((getter, props) => getter(props));
+const log = props => tap(getFieldFromProps(getWriteLog)(props));
+
+const getBinaryNumber = props => compose(makeBinaryNumberResponse, log(props), round);
+const getAnimalName = props => compose(
+    makeAnimalNameResponse,
+    log(props),
+    modulo(__, 3),
+    log(props),
+    powSquared,
+    log(props),
+    getLength,
+    log(props),
+    getResult
+);
+const sendSuccessName = props => compose(getHandleSuccess(props), getResult);
+
+const processSequence = props => {
+    pipe(
+        then(log(props)),
+        then(checkInvalidString),
+        then(getBinaryNumber(props)),
+        then(getAnimalName(props)),
+        thenWithCatch(sendSuccessName(props), getHandleError(props))
+    )(getPromiseResolve(getValue(props)));
+}
+
+export default processSequence;
